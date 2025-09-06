f6d69c1bca0bba145cdb7ada6c70d6a9eb609929
Anshuman Samanta <132367367+Anshuman22coder@users.noreply.github.com>
2025-09-06 22:52:32 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,146 +1,113 @@
 import express from "express"
 import { Server } from "socket.io"
-import {createServer} from "http"
-const app=express()
+import { createServer } from "http"
+const app = express()
 
-import {Users,Messages,connectDb} from "./db.js"
+import { Users, Messages, connectDb } from "./db.js"
 
-
-
-
-const server=createServer(app);//the whole server is instantiated ..with http
-const io=new Server(server,{
-  cors:{
-    origin:"*",
-    methods:["GET","POST"],
-    credentials:true
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true
   }
-}); //the whole io circut is instantaited..
-const userSocketmap={} //userId:socket.id
+});
+const userSocketmap = {} // userId: socket.id
 
-io.on("connection",async(socket)=>{  //listens .. to incomming connections..
-  //1.client connects
-  const userId=socket.handshake.query.userId;
+io.on("connection", async (socket) => {
+  const userId = socket.handshake.query.userId;
 
-  if(userId)
-  {
-    userSocketmap[userId]=socket.id;
-    //mongoDb id storing
-    const user=await Users.findOne({"id":userId});
-    if(user){
-      console.log("User found",user.id)
+  if (userId) {
+    userSocketmap[userId] = socket.id;
+
+    // Check and create a new user in DB if not found
+    const user = await Users.findOne({ "id": userId });
+    if (user) {
+      console.log("User found", user.id);
+    } else {
+      await Users.create({ "id": userId });
+      console.log("New USER created in db");
     }
-    else
-    {
-      await Users.create({"id":userId});
-      console.log("new USER created in db")
-    }
-
-
-
 
     console.log(`User ${userId} connected with socket ${socket.id}`);
 
+    // Send chat history to the client
+    const history = await Messages.find({
+      $or: [
+        { "receiver": userId },
+        { "sender": userId }
+      ]
+    })
+      .sort({ currentDate: -1 })
+      .limit(5);
 
-
-
-    //sending of user datas to the client...
-   const history = await Messages.find({
-  $or: [
-    {"receiver": userId},
-    {"sender": userId}
-  ]
-})
-        .sort({currentDate:-1})   //ascending order..latest 5 picked up
-        .limit(5)
-        history.reverse();      //they are reversed now..
-    socket.emit("chat-history",history)
+    history.reverse();
+    socket.emit("chat-history", history);
   }
 
   console.log("User Connected");
-  console.log("Id",socket.id);
+  console.log("Id", socket.id);
 
-  //socket.emit("welcome",`welcome to the server ${socket.id}`);
-  socket.on("message",({room,message})=>{ // 2. message received from client
-    console.log({room,message});//triggered by frontend when button of form is submitted ,,
-   socket.to(room).emit("recieve-message", { message,sender: userId ,room}); 
-    /*{
-  message: "hello",
-  sender: "abc123"
-}*/
+  socket.on("message", ({ room, message }) => {
+    console.log({ room, message });
+    socket.to(room).emit("recieve-message", { message, sender: userId, room });
+  });
 
-  })
+  socket.on("message_Individual", async ({ room, message }) => {
+    const currentDate = new Date();
+    const receiverSocketId = userSocketmap[room.trim()];
 
-   
+    if (receiverSocketId) {
+      try {
+        const savedMessage = await Messages.create({
+          receiver: room,
+          sender: userId,
+          message: message,
+          currentDate: currentDate
+        });
 
-
-
-
-
-  let sendervalue;
-    socket.on("message_Individual",async({room,message})=>{ // 2. message received from client
-      const currentDate=new Date().toLocaleTimeString();
-      
-      if((Object.entries(userSocketmap).length)>1)
-        console.log("PRESENT")
-      for (let [key,value] of Object.entries(userSocketmap))  //objects to arrays..
-      { key=key.trim()
-        if(key===room.trim())
-        {
-           sendervalue=value;
-           console.log("I entered here")
-           break;
-        }
-        console.log({"key":key})
+        console.log({ room, message });
         
+        // Emit the message to the correct receiver's socket
+        socket.to(receiverSocketId).emit("recieve-message", {
+          id: savedMessage.receiver,
+          sender: savedMessage.sender,
+          message: savedMessage.message,
+          currentDate: savedMessage.currentDate
+        });
+        
+      } catch (error) {
+        console.log("Could not perform DB transactions for message storage", error);
       }
-      //save to Db
-      try{
-        const savedMessage=await Messages.create({
-        receiver:room,
-        sender:userId,
-        message:message,
-        currentDate:new Date()
-        
-      })
-       console.log({room,message});//triggered by frontend when button of form is submitted ,,
-   socket.to(sendervalue).emit("recieve-message", {id:savedMessage.receiver, sender: savedMessage.sender,message:savedMessage.message, currentDate:savedMessage.currentDate}); 
-    
+    } else {
+        console.log(`User with ID ${room} is not currently online.`);
     }
-      catch(error){
-      console.log("could not do the db transcations of msg storing ",error)
-      }
-      
-    
+  });
 
-
-   
-    /*{
-  message: "hello",
-  sender: "abc123"
-}*/
-
-  })
-  
-
-  
-  // 3. client wants to join a room
-  socket.on("join-room",(room)=>{
+  socket.on("join-room", (room) => {
     socket.join(room);
     console.log(`User joined room ${room}`);
-  })
-  
-  socket.on("disconnect",()=>{  //triggered from frontend
-    console.log("User disconnected",socket.id);
-  })
-})
+  });
 
-app.get("/",(req,res)=>{
-  res.send("HI")
-})
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+    // You might want to remove the user from userSocketmap here
+    for (const key in userSocketmap) {
+        if (userSocketmap[key] === socket.id) {
+            delete userSocketmap[key];
+            break;
+        }
+    }
+  });
+});
 
-connectDb().then(()=>{
-server.listen (4000,()=>{
-  console.log('server listening at 4000')
-})
-})
+app.get("/", (req, res) => {
+  res.send("Server is running.");
+});
+
+connectDb().then(() => {
+  server.listen(4000, () => {
+    console.log('Server listening at 4000');
+  });
+});
